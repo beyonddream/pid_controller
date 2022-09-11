595c84c61e6fdb097a431dfdb847783ef2cb56d0
beyonddream <43626691+beyonddream@users.noreply.github.com>
2022-09-11 10:51:13 -0700

    * add basic api's

--- pid.py
+++ pid.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python3
 
+from copy import copy
+import time
+
 from typing import AnyStr
 
 class PIDController:
 
     class PIDState:
 
-        def __init__(self, kP : int, kI: int, kD: int) -> None:
-            self.kP = kP
-            self.kI = kI
-            self.kD = kD
+        def __init__(self, output: float = 0, prev_error: float = 0, last_updated_ns: int = time.monotonic()) -> None:
+            self._output = output
+            self._prev_error = prev_error
+            self._last_updated_ns = last_updated_ns
 
         def dump(self) -> AnyStr:
-            return f"kP = {self.kP}, kI = {self.kI}, kD = {self.kD}"
+            return f"output = {self._output}, prev_error = {self._prev_error}, last_updated_ns = {self._last_updated_ns}"
 
-    def __init__(self, kP : float, kI: float, kD: float) -> None:
-        self.state = PIDController.PIDState(kP, kI, kD)
+        def __str__(self) -> AnyStr:
+            return self.dump()
 
+    def __init__(self, kP : float, kI: float, kD: float, target: float) -> None:
+        self._kP = kP
+        self._kI = kI
+        self._kD = kD
+        self._target = target
+        self._state = PIDController.PIDState()
 
     def dump(self) -> AnyStr:
-        return self.state.dump()
+        return f"kP = {self._kP}, kI = {self._kI}, kD = {self._kD}, target = {self._target}, {self._state.dump()}"
 
+    def update(self, current: float) -> None:
+        pass
+
+    def get(self) -> PIDState:
+        return copy(self._state)
+
+    def __str__(self) -> AnyStr:
+        return self.dump()
 
 if __name__ == '__main__':
-    print(PIDController(1.3, 3, 4).dump())
+    pid = PIDController(1.3, 3, 4, 1000)
+    print(pid)
+    print(pid.get())
